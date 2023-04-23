787b9b5cc0f7ad7565733b71c26320cb787882f8
Giang Truong <minhgiang1204@gmail.com>
2023-04-23 23:56:18 +0700

    Add project files.

---
+++ learntoshare.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace learntoshare.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
